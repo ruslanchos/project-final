1f551227f18ec06ffa786170133267eb99b11282
Ruslan A.Bityutskih <r.bituckih@gmail.com>
2023-04-26 19:02:39 +0300

    005 - encoding: UTF-8

--- src/test/java/com/javarush/jira/JiraRushApplicationTests.java
+++ src/test/java/com/javarush/jira/JiraRushApplicationTests.java
@@ -2,8 +2,14 @@ package com.javarush.jira;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 
 @SpringBootTest
+@Sql(scripts = {"classpath:db/changelog/init-schema.sql", "classpath:db/test.sql"}, config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles("test")
+
 class JiraRushApplicationTests {
 	@Test
 	void contextLoads() {
